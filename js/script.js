1640142d1aa887ca7252d091af9ee812bdcae8a3
TimShevy <126278688+TimShevy@users.noreply.github.com>
2023-03-06 15:15:21 +0500

    Переделал в одну функцию
    Функция validatePhone(num) проверяет номер телефона на соответствие операторам связи Узбекистана, а так же выводит номер в формате: +998(XX) XXX-XX-XX

--- js/script.js
+++ js/script.js
@@ -1,30 +1,18 @@
 function validatePhone(num) {
-    let regex = /^((998|\+998)[\- ]?)\(?\d{2}\)?[\- ]?\d{3}[\- ]?\d{2}[\- ]?\d{2}$/;
-    return regex.test(num);
-   }
-
-function formatPhone(num) {
     num = num.replace(/[^\d]/g, '');
-    if (num.length === 12) {
+    let regex = /^((998)\d{9})$/;
+    if (regex.test(num)) {
         let countryCode = num.slice(0, 3);
         let operatorCode = num.slice(3, 5);
         let triade = num.slice(5, 8);
         let pair1 = num.slice(8, 10);
         let pair2 = num.slice(10, 12);
-        return (`+${countryCode}(${operatorCode}) ${triade}-${pair1}-${pair2}`);
+        return num = (`+${countryCode}(${operatorCode}) ${triade}-${pair1}-${pair2}`);
     }
 }
-
 let num = prompt('Введите номер телефона любого оператора Узбекистан: ')
-
-// if (validatePhone(num)) {
-//        console.log('Соответствует');
-//     } else {
-//        console.log('Не соответствует');
-//     }
-
 if (validatePhone(num)) {
-    alert(`Ваш номер: ${formatPhone(num)}. Спасибо! Мы Вам перезвоним!`)
+    alert(`Ваш номер: ${validatePhone(num)}. Спасибо! Мы Вам перезвоним!`)
 } else { 
     alert('Введенные данные не соответствуют номеру телефона оператора Узбекистана. Введите заново в правильном формате, пожалуйста!')
 }
